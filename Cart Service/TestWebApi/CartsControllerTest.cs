ece5ba5bf77f48898641adb7b80d9bb585ac4df2
Ihor Cherevko <Ihor_Cherevko@epam.com>
2024-10-18 23:21:23 +0300

    added integration test

--- Cart Service/TestWebApi/CartsControllerTest.cs
+++ Cart Service/TestWebApi/CartsControllerTest.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using LiteDB;
 using Microsoft.Extensions.Logging;
 using Moq;
 using OnlineShopping.CartService.WebApi.BLL;
+using OnlineShopping.CartService.WebApi.DAL;
+using OnlineShopping.CartService.WebApi.DAL.Entities;
 using OnlineShopping.CartService.WebApi.UI.Controllers;
 
 namespace TestWebApi
@@ -10,7 +13,7 @@ namespace TestWebApi
     internal class CartsControllerTest
     {
         [Test]
-        public async Task DeleiteMethdTest()
+        public void DeleiteMethdTest()
         {
             // Arrange
             var mockCartItemsService = new Mock<ICartItemsService>();
@@ -19,6 +22,34 @@ namespace TestWebApi
 
             var controller = new CartsController(Mock.Of<ILogger<CartsController>>()
                 , mockCartItemsService.Object
+                , Mock.Of<IMapper>());
+
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.True(result);
+        }
+
+
+        [Test]
+        public void DeleiteMethd_Integration_Test()
+        {
+            // Arrange
+            var mockLiteCollection = new Mock<ILiteCollection<CartItem>>();
+            mockLiteCollection.Setup(x => x.Delete(It.IsAny<BsonValue>()))
+                .Returns(true);
+            var mockLiteDatabase = new Mock<ILiteDatabase>();
+            mockLiteDatabase.Setup(x => x.GetCollection<CartItem>(It.IsAny<string>(), It.IsAny<BsonAutoId>()))
+                .Returns(mockLiteCollection.Object);
+
+            var mockLiteDbContext = new Mock<ILiteDbContext>();
+            mockLiteDbContext.Setup(c => c.Database)
+                .Returns(mockLiteDatabase.Object);
+            var cartItemsService = new CartItemsService(mockLiteDbContext.Object);
+
+            var controller = new CartsController(Mock.Of<ILogger<CartsController>>()
+                , cartItemsService
                 , Mock.Of<IMapper>());
 
             // Act
