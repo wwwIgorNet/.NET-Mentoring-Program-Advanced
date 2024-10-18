2bb427165675b23c1dd5409f65957bf39811356d
Ihor Cherevko <Ihor_Cherevko@epam.com>
2024-10-18 22:40:41 +0300

    added tests

---
+++ Cart Service/TestWebApi/CartsControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OnlineShopping.CartService.WebApi.BLL;
+using OnlineShopping.CartService.WebApi.UI.Controllers;
+
+namespace TestWebApi
+{
+    [TestFixture]
+    internal class CartsControllerTest
+    {
+        [Test]
+        public async Task DeleiteMethdTest()
+        {
+            // Arrange
+            var mockCartItemsService = new Mock<ICartItemsService>();
+            mockCartItemsService.Setup(service => service.Delete(It.IsAny<int>()))
+                .Returns(true);
+
+            var controller = new CartsController(Mock.Of<ILogger<CartsController>>()
+                , mockCartItemsService.Object
+                , Mock.Of<IMapper>());
+
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
